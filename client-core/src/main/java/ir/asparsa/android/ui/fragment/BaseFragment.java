45aa317c26d3d0acf474d5b48e37b8fb5ba5457f
Hadi <hadilashkari@gmail.com>
2016-12-15 22:49:09 +0330

    Implement base dialog fragment

--- client-core/src/main/java/ir/asparsa/android/ui/fragment/BaseFragment.java
+++ client-core/src/main/java/ir/asparsa/android/ui/fragment/BaseFragment.java
@@ -1,5 +1,8 @@
 package ir.asparsa.android.ui.fragment;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 
 /**
@@ -11,4 +14,36 @@ public abstract class BaseFragment extends Fragment {
     public String getTagName() {
         return getClass().getName();
     }
+
+    public void onEvent(BaseEvent event) {
+    }
+
+    public static abstract class BaseEvent implements Parcelable {
+        private String mSourceTag;
+
+        public BaseEvent(@NonNull String sourceTag) {
+            this.mSourceTag = sourceTag;
+        }
+
+        public String getSourceTag() {
+            return mSourceTag;
+        }
+
+
+        @Override public int describeContents() {
+            return 0;
+        }
+
+        @Override public void writeToParcel(
+                Parcel dest,
+                int flags
+        ) {
+            dest.writeString(this.mSourceTag);
+        }
+
+        protected BaseEvent(Parcel in) {
+            this.mSourceTag = in.readString();
+        }
+    }
+
 }
---
+++ client-core/src/main/java/ir/asparsa/android/ui/fragment/dialog/BaseDialogFragment.java
@@ -0,0 +1,96 @@
+package ir.asparsa.android.ui.fragment.dialog;
+
+import android.content.DialogInterface;
+import android.os.Parcel;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import ir.asparsa.android.core.logger.L;
+import ir.asparsa.android.ui.fragment.BaseFragment;
+
+import java.util.List;
+
+/**
+ * Created by hadi on 12/15/2016 AD.
+ */
+public abstract class BaseDialogFragment extends DialogFragment {
+
+    private static final String BUNDLE_KEY_DIALOG_RESULT_EVENT = "BUNDLE_KEY_DIALOG_RESULT_EVENT";
+
+    public void setOnDialogResultEvent(BaseOnDialogResultEvent event) {
+        getArguments().putParcelable(BUNDLE_KEY_DIALOG_RESULT_EVENT, event);
+    }
+
+    protected BaseOnDialogResultEvent getOnDialogResultEvent() {
+        return getArguments().getParcelable(BUNDLE_KEY_DIALOG_RESULT_EVENT);
+    }
+
+    protected void sendEvent() {
+        BaseOnDialogResultEvent event = getOnDialogResultEvent();
+
+        List<Fragment> fragments = getFragmentManager().getFragments();
+        for (int i = fragments.size() - 1; i >= 0; i--) {
+            Fragment fragment = fragments.get(i);
+            if (event.getSourceTag().equals(fragment.getTag()) && fragment instanceof BaseFragment) {
+                L.i(this.getClass(), "Find base fragment to send event: " + fragment.getClass().getName());
+                ((BaseFragment) fragment).onEvent(event);
+                break;
+            }
+        }
+    }
+
+    @Override public void onCancel(DialogInterface dialog) {
+        getOnDialogResultEvent().setDialogResult(DialogResult.CANCEL);
+        sendEvent();
+        super.onCancel(dialog);
+    }
+
+    public void show(
+            FragmentManager manager
+    ) {
+        super.show(manager, getClass().getName());
+    }
+
+    public static abstract class BaseOnDialogResultEvent extends BaseFragment.BaseEvent {
+
+        private int mDialogResult = -1;
+
+        public BaseOnDialogResultEvent(@NonNull String sourceTag) {
+            super(sourceTag);
+        }
+
+        public int getDialogResult() {
+            return mDialogResult;
+        }
+
+        public void setDialogResult(int dialogResult) {
+            this.mDialogResult = dialogResult;
+        }
+
+
+        @Override public int describeContents() {
+            return 0;
+        }
+
+        @Override public void writeToParcel(
+                Parcel dest,
+                int flags
+        ) {
+            super.writeToParcel(dest, flags);
+            dest.writeInt(this.mDialogResult);
+        }
+
+        protected BaseOnDialogResultEvent(Parcel in) {
+            super(in);
+            this.mDialogResult = in.readInt();
+        }
+    }
+
+    public interface DialogResult {
+        int COMMIT = 0;
+        int NEUTRAL = 1;
+        int CANCEL = 2;
+    }
+
+}
