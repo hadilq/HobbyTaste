3c24d6092b471505719760119e2d4294107ef426
Hadi <hadilashkari@gmail.com>
2017-01-12 21:13:12 +0330

    Replace dialog fragment with bottom sheet dialog fragment

--- client-core/src/main/java/ir/asparsa/android/ui/fragment/dialog/BaseDialogFragment.java
+++ client-core/src/main/java/ir/asparsa/android/ui/fragment/dialog/BaseDialogFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Parcel;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -20,7 +21,7 @@ import java.util.List;
 /**
  * Created by hadi on 12/15/2016 AD.
  */
-public abstract class BaseDialogFragment extends DialogFragment {
+public abstract class BaseDialogFragment extends BottomSheetDialogFragment {
 
     private static final String BUNDLE_KEY_DIALOG_RESULT_EVENT = "BUNDLE_KEY_DIALOG_RESULT_EVENT";
 
