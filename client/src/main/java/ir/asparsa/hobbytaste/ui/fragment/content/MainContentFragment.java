88c573d64560fae5d91002f854ec2ace49ea9505
Hadi <hadilashkari@gmail.com>
2017-03-16 19:52:26 +0330

    Toast error if loading stores gets error

--- client/src/main/java/ir/asparsa/hobbytaste/ui/fragment/content/MainContentFragment.java
+++ client/src/main/java/ir/asparsa/hobbytaste/ui/fragment/content/MainContentFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -103,6 +104,7 @@ public class MainContentFragment extends BaseContentFragment
 
             @Override public void onError(Throwable e) {
                 L.w(MainContentFragment.class, "Store gets error", e);
+                Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
 
             @Override public void onNext(Collection<StoreModel> stores) {
