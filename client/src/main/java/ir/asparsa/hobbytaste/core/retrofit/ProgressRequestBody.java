13e5f55355985c9de0ef79d8546f8695413ec349
Hadi <hadilashkari@gmail.com>
2017-04-11 17:15:54 +0430

    Make progress bar more smooth

--- client/src/main/java/ir/asparsa/hobbytaste/core/retrofit/ProgressRequestBody.java
+++ client/src/main/java/ir/asparsa/hobbytaste/core/retrofit/ProgressRequestBody.java
@@ -4,8 +4,10 @@ import ir.asparsa.android.core.logger.L;
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okio.BufferedSink;
+import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.subjects.PublishSubject;
 import rx.subjects.SerializedSubject;
 import rx.subjects.Subject;
@@ -13,6 +15,7 @@ import rx.subjects.Subject;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class ProgressRequestBody extends RequestBody {
     private File mFile;
@@ -30,7 +33,12 @@ public class ProgressRequestBody extends RequestBody {
 
     public ProgressRequestBody registerObserver(Action1<Integer> observer) {
         mOnUploadProgressSubject
-                .onBackpressureDrop()
+                .onBackpressureBuffer()
+                .concatMap(new Func1<Integer, Observable<Integer>>() {
+                    @Override public Observable<Integer> call(Integer i) {
+                        return Observable.just(i).delay(50, TimeUnit.MILLISECONDS);
+                    }
+                })
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(observer, new Action1<Throwable>() {
                     @Override public void call(Throwable throwable) {
@@ -60,13 +68,14 @@ public class ProgressRequestBody extends RequestBody {
 
         try {
             int read;
-            int percentage = 0;
+            int percentage;
             int lastPercentage = 0;
             while ((read = in.read(buffer)) != -1) {
 
                 // update progress on UI thread
                 percentage = (int) (100 * uploaded / fileLength);
-                if (percentage % 10 == 0 && percentage != lastPercentage) {
+                if (percentage % 5 == 0 && percentage != lastPercentage) {
+                    L.i(ProgressRequestBody.class, "The progress percentage: " + percentage);
                     mOnUploadProgressSubject.onNext(percentage);
                     lastPercentage = percentage;
                 }
