e3290abf69782a414c51d13ccfddeeaa2b81a564
Hadi <hadilashkari@gmail.com>
2017-04-28 21:49:00 +0430

    Remove unnecessary reflection

--- client-core/src/main/java/ir/asparsa/android/ui/view/NonSwipeableViewPager.java
+++ client-core/src/main/java/ir/asparsa/android/ui/view/NonSwipeableViewPager.java
@@ -4,10 +4,6 @@ import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.Scroller;
-
-import java.lang.reflect.Field;
 
 /**
  * @author hadi
@@ -17,12 +13,13 @@ public class NonSwipeableViewPager extends ViewPager {
 
     public NonSwipeableViewPager(Context context) {
         super(context);
-        setMyScroller();
     }
 
-    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+    public NonSwipeableViewPager(
+            Context context,
+            AttributeSet attrs
+    ) {
         super(context, attrs);
-        setMyScroller();
     }
 
     @Override
@@ -35,28 +32,5 @@ public class NonSwipeableViewPager extends ViewPager {
     public boolean onTouchEvent(MotionEvent event) {
         // Never allow swiping to switch between pages
         return false;
-    }
-
-    //down one is added for smooth scrolling
-    private void setMyScroller() {
-        try {
-            Class<?> viewpager = ViewPager.class;
-            Field scroller = viewpager.getDeclaredField("mScroller");
-            scroller.setAccessible(true);
-            scroller.set(this, new MyScroller(getContext()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public class MyScroller extends Scroller {
-        MyScroller(Context context) {
-            super(context, new DecelerateInterpolator());
-        }
-
-        @Override
-        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
-            super.startScroll(startX, startY, dx, dy, 350 /*1 secs*/);
-        }
     }
 }
